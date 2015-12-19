5ba6f685772fc0f6005e1aace4d2febc10f6d17a
Clarence Risher <sparr0@gmail.com>
2015-12-18 21:46:05 -0800

    handle more types of error terms from erl_lint:module, also commenting

--- src/erlint.erl
+++ src/erlint.erl
@@ -15,22 +15,38 @@ lint(File) ->
     case epp:parse_file(File,[]) of
         {ok, Forms} ->
             case erl_lint:module(Forms, File) of
+                % nothing wrong
                 {ok, []} ->
                     [];
+                % just warnings
                 {ok, [Warnings]} ->
                     [{warning,element(1,Warnings),X} || X <- element(2,Warnings)];
+                % errors, no warnings
+                {error, [Errors], []} ->
+                    [{error,element(1,Errors),X} || X <- element(2,Errors)];
+                % errors and warnings
                 {error, [Errors], [Warnings]} ->
-                        [{error,element(1,Errors),X} || X <- element(2,Errors)] ++
-                        [{warning,element(1,Warnings),X} || X <- element(2,Warnings)]
+                    [{error,element(1,Errors),X} || X <- element(2,Errors)] ++
+                    [{warning,element(1,Warnings),X} || X <- element(2,Warnings)]
             end;
         {error, _} ->
             "Cannot open file"
     end.
 
+% degenerate case
 print([]) ->
     0;
+% some errors use a tuple to describe the error
+print([{Type,File,{Line,_Module,{ErrT,ErrX}}}|More]) ->
+    io:fwrite("~s:~w: ~w: ~w ~w~n",[File,Line,Type,ErrT,ErrX]),
+    print(More);
+% some errors use a list to describe the error
+print([{Type,File,{Line,_Module,[ErrT,ErrX]}}|More]) ->
+    io:fwrite("~s:~w: ~w: ~s ~s~n",[File,Line,Type,ErrT,ErrX]),
+    print(More);
+% some errors use a string to describe the error
 print([{Type,File,{Line,_Module,Err}}|More]) ->
-    io:fwrite("~s:~w: ~w: ~w~n",[File,Line,Type,Err]),
+    io:fwrite("~s:~w: ~w: ~s~n",[File,Line,Type,Err]),
     print(More);
 print(String) ->
     io:fwrite("~s~n",[String]).
